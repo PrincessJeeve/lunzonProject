c24210624ff1525ac18f6ad194d2d2e812c95a97
Jeeve Lunzon <jeevelyn.lunzon@bisu.edu.ph>
2023-09-04 23:27:50 +0800

    updated

--- App.js
+++ App.js
@@ -36,13 +36,14 @@ const Subject = props => {
 
 const Cname = props => {
   return (
-    <Text style={styles.bodyFont}>{props.details}</Text>
+      <Text style={styles.bodyFont}>{props.details}</Text>
   )
 };
 
 const Unit = props => {
   return(
-    <Text style={styles.bodyFont}>UNIT: {props.num}</Text>
+       <Text style={styles.bodyFont}>UNIT: {props.num}</Text>
+   
   )
 }
 
@@ -58,37 +59,44 @@ export default function App() {
       <Text style={styles.mySubFont}>My Subjects</Text>
 
       <View style={styles.subHeader}>
-      <View>
-      <Subject list = 'ELEC 1'/>
-        <View> 
-          <Cname details = 'Web Development/Web Enterprise'/>
-          <Unit num = '3'/>
+       <Subject list = 'ELEC 1'/>
+        <View>
+        <Cname details = 'Web Development/Web Enterprise'/>
+        <Unit num = '3'/>
         </View>
-        </View>  
       </View>
+       
 
       <View style={styles.subHeader}>
        <Subject list = 'ELEC 3'/>
-       <Cname details = 'Mobile Application'/>
-       <Unit num = '3'/>
+       <View>
+        <Cname details = 'Mobile Application'/>
+        <Unit num = '3'/>
+       </View>
       </View>
 
       <View style={styles.subHeader}>
        <Subject list = 'IT 311'/>
-       <Cname details = 'Software Engineeing'/>
-       <Unit num = '3'/>
+       <View>
+        <Cname details = 'Software Engineeing'/>
+        <Unit num = '3'/>
+       </View>
       </View>
 
       <View style={styles.subHeader}>
        <Subject list = 'IT 312'/>
-       <Cname details = 'Information Assurance and Security 2'/>
-       <Unit num = '3'/>
+        <View>
+        <Cname details = 'Information Assurance and Security 2'/>
+        <Unit num = '3'/>
+        </View>
       </View>
 
       <View style={styles.subHeader}>
        <Subject list = 'IT 313'/>
-       <Cname details = 'Quantitative Methods'/>
-       <Unit num = '3'/>
+       <View>
+        <Cname details = 'Quantitative Methods'/>
+        <Unit num = '3'/>
+       </View>
       </View>
 
       <Text style={styles.clickHereFont}>Click here to see more</Text>
@@ -100,12 +108,9 @@ export default function App() {
 const styles = StyleSheet.create({
   background: {
     marginLeft: '10px',
-    backgroundColor: '#F0FFFF'
+    backgroundColor: 'white'
   },
   header: {
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'flex-start',
       backgroundColor: '#24469C',
       alignItems: 'left',
       justifyContent: 'center',
@@ -135,15 +140,19 @@ const styles = StyleSheet.create({
     marginTop: '5px'
   },  
   subHeader: {
-    backgroundColor: '#DADBDD',
-    opacity: 0.7,
-    borderTopRightRadius: 10,
-    borderTopLeftRadius: 10,
-    borderBottomRightRadius: 10,
-    borderBottomLeftRadius: 10,
-    marginBottom:'20px',
-    marginLeft: '15px',
-    marginRight: '15px'
+    flex: 1,
+   flexDirection: 'row',
+   justifyContent: 'flex-start',
+   alignItems: 'center',
+   backgroundColor: '#DCDCDC',
+   borderTopRightRadius: 10,
+   borderTopLeftRadius: 10,
+   borderBottomRightRadius: 10,
+   borderBottomLeftRadius: 10,
+   marginBottom:'8px',
+   marginTop:'8px',
+   marginRight: '20px',
+   marginLeft: '20px'
   },
   subHeaderFont: {
     color: 'black',
@@ -153,15 +162,17 @@ const styles = StyleSheet.create({
   },
   bodyFont: {
     fontFamily: 'Arial',
-    fontSize: '10px',
+    fontSize: '12px',
     color: 'black',
-    textAlign: 'right',
-    marginLeft: '20px',
-    marginTop: '5px'
+    textAlign: 'left',
+    marginTop: '5px',
+    marginLeft: '30px'
   },
   mySubFont: {
+    fontSize: '20px',
     marginBottom: '5px',
-    marginLeft: '20px'
+    marginLeft: '20px',
+    
   },
   clickHereFont: {
     color: '#24469C',
